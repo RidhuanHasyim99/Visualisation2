faec1dce54adfa465049aefe89b8e3c6629121b9
RidhuanHasyim99 <111556631+RidhuanHasyim99@users.noreply.github.com>
2022-10-17 22:21:01 +1100

    Add files via upload

---
+++ test.js
@@ -0,0 +1,53 @@
+var vg_1 = "map.json";
+vegaEmbed("#chart", vg_1,{"actions": false}).then(function(result) {
+// Access the Vega view instance
+//(https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var vg_2 = "forestloss.json";
+vegaEmbed("#forestchart", vg_2,{"actions": false}).then(function(result) {
+// Access the Vega view instance
+//(https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var vg_3 = "driversforestloss.json";
+vegaEmbed("#driversforestchart", vg_3,{"actions": false}).then(function(result) {
+// Access the Vega view instance
+//(https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var vg_4 = "temperature.json";
+vegaEmbed("#tempchart", vg_4,{"actions": false}).then(function(result) {
+// Access the Vega view instance
+//(https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var vg_5 = "airsource.json";
+vegaEmbed("#airchart", vg_5,{"actions": false}).then(function(result) {
+// Access the Vega view instance
+//(https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var vg_6 = "airemission.json";
+vegaEmbed("#airemissionchart", vg_6,{"actions": false}).then(function(result) {
+// Access the Vega view instance
+//(https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var vg_7 = "rotating.json";
+vegaEmbed("#rotatechart", vg_7,{"actions": false}).then(function(result) {
+// Access the Vega view instance
+//(https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var vg_8 = "watersource.json";
+vegaEmbed("#waterchart", vg_8,{"actions": false}).then(function(result) {
+// Access the Vega view instance
+//(https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var vg_9 = "nocleanwater.json";
+vegaEmbed("#nocleanwaterchart", vg_9,{"actions": false}).then(function(result) {
+// Access the Vega view instance
+//(https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
